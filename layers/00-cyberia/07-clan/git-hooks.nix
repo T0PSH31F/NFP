ccee07fc1b2e8e5a5d9966a1828efd2cad407266
t0psh31f <wrighterik77@gmail.com>
2026-09-09 06:10:01 -0700

    fix(hooks): include nami in pre-push nix-eval-toplevels verification

--- layers/00-cyberia/07-clan/git-hooks.nix
+++ layers/00-cyberia/07-clan/git-hooks.nix
@@ -32,12 +32,14 @@
           nix-eval-toplevels = {
             enable = true;
             name = "Fast nix eval of machine toplevels";
-            entry = "${pkgs.writeShellScript "nix-eval-toplevels" ''
-              set -euo pipefail
-              exec >/dev/null 2>&1
-              nix eval .#nixosConfigurations.luffy.config.system.build.toplevel.drvPath
-              nix eval .#nixosConfigurations.z0r0.config.system.build.toplevel.drvPath
-            ''}";
+            entry = toString (
+              pkgs.writeShellScript "nix-eval-toplevels" ''
+                set -euo pipefail
+                nix eval --raw .#nixosConfigurations.luffy.config.system.build.toplevel.drvPath >/dev/null 2>&1
+                nix eval --raw .#nixosConfigurations.z0r0.config.system.build.toplevel.drvPath >/dev/null 2>&1
+                nix eval --raw .#nixosConfigurations.nami.config.system.build.toplevel.drvPath >/dev/null 2>&1
+              ''
+            );
             stages = [ "pre-push" ];
             pass_filenames = false;
           };
