6b9df6b050a839ba1a8f0bdccb423851f52f39e8
t0psh31f <wrighterik77@gmail.com>
2026-09-12 15:26:35 -0700

    fix(caches): add luffy LAN (192.168.1.54) and VPN Harmonia binary cache to substituters

--- layers/10-system/11-foundation/caches.nix
+++ layers/10-system/11-foundation/caches.nix
@@ -23,6 +23,8 @@ _: {
       # mic92.cachix.org removed 2026-08-07: timed out during builds (progress.md).
       substituters = [
         "https://cache.nixos.org"
+        "http://192.168.1.54:5000"
+        "http://100.72.46.75:5000"
         "https://nix-community.cachix.org"
         "https://numtide.cachix.org"
         "https://vicinae.cachix.org"
@@ -36,6 +38,7 @@ _: {
 
       # Set the trusted public keys for the substituters above
       trusted-public-keys = [
+        "nix-cache-1:YhXcvDzqzmRyP4QCsHbH67iYcX7L0fGUt7dNfEGzJz0="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
